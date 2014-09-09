199d591bdb6096e149947987bc87fad53829a055
Philipp <philippa@bitvid.tv>
2014-09-09 20:19:45 +0200

    only listen on cersei-web-2.bitvid.tv

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config:
 
 
 class DevConfig(Config):
-    HOST = "http://localhost:8000"
+    HOST = "" #http://localhost:8000"
 
 class PLGConfig(DevConfig):
     API_URL = "http://cersei-web-2.bitvid.tv:5000"
-    HOST = "http://playground.bitvid.tv"
+    HOST = "" #http://cersei-web-2.bitvid.tv/"
