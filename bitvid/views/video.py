a7626e6f01cc82fc4430312e2d5a87ed3bec99f5
Philipp <philippa@bitvid.tv>
2014-11-05 21:20:14 +0100

    failed to load index page and video page when index is empty

--- bitvid/views/video.py
+++ bitvid/views/video.py
@@ -2,7 +2,7 @@
 
 from pprint import pprint
 
-from flask import g, render_template, request
+from flask import g, render_template, request, flash, redirect, url_for
 from flask.ext.classy import FlaskView
 
 from bitvid import app
@@ -14,15 +14,24 @@ class VideoView(FlaskView):
     def index(self):
         query = request.args.get("q", "*")
         curpage = request.args.get("page", 0)
-        pages, videos = video_query(query, page= curpage)
+        query_result = video_query(query, page= curpage)
+
+        pages, videos = 1, {}
+        if len(query_result) == 2:
+            pages, videos = query_result
 
         return render_template("videolist.html", videos=videos, pages=pages, query=query)
 
     def get(self, videoid):
-        video = video_query("token:" + videoid)[0]
-        comments = g.client.getCommentsForVideo(videoid)
-        pprint(video, indent=4)
-        return render_template("video.html", video=video,
-                               videoMedias=video["medias"], comments=comments)
+        try:
+            video = video_query("token:" + videoid)[1][0]
+            comments = g.client.getCommentsForVideo(videoid)
+            pprint(video, indent=4)
+            return render_template("video.html", video=video,
+                                   videoMedias=video["medias"], comments=comments)
+        except Exception as e:
+            flash("failed to load video")
+            print e
+            return redirect(app.config["HOST"] + url_for('VideoView:index'))
 
 VideoView.register(app)
